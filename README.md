bb7c2c800e420637803a44844acfe34677bf70c1
carlos montenegro <cmontenegro@strongboxdata.com>
2017-08-09 01:04:11 -0600

    Added all the gathering methods and added documentation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # logs-analysis
 Logs analyzer for a newspaper website.
+
+## Steps to run the application:
+
+### Create the following views in you database
+
+```create view a1 as select to_char(time,'FMMonth FMDD, YYYY'), count(status) as total from log
+where status = '404 NOT FOUND'
+group by to_char(time,'FMMonth FMDD, YYYY')
+;```
+
+```create view r2 as select to_char(time,'FMMonth FMDD, YYYY'), count(status) as total from log
+group by to_char(time,'FMMonth FMDD, YYYY')
+;```
+
+```create view rate_val as
+select r2.to_char, cast(a1.total*100 as float)/ cast(r2.total as float) as val from a1,r2 where a1.to_char = r2.to_char;```
+
+### Run the following file
+
+```python3 analyzer.py```
--- analyzer.py
+++ analyzer.py
@@ -30,16 +30,32 @@ class QueryProcessor(LogsAnalyzer):
                     (regexp_split_to_array(path, E'/article/'))[2],
                     articles.title
                     order by views desc limit 3 ;"""
+        self._execute_query(query)
+
+    def get_most_popular_authors(self):
+        """ Method that retrieves the most popular authors
+            of all time.
+        """
+        query = """ select authors.name, count(log.path) as views from authors
+                    left join articles on authors.id = articles.author
+                    left join log on log.path like concat('%', articles.slug)
+                    group by authors.name order by views desc ;"""
+        self._execute_query(query)
+
+    def get_days_with_most_errors(self):
+        """ Method that retrieves days with a failure rate greater
+            than 1%.
+        """
+        query = "select * from rate_val where val > 1;"
+        self._execute_query(query)
+
+    def _execute_query(self, query):
+        """ Private method that does the actual query processing
+        """
         # Sending the query to be run
         self.cursor.execute(query)
         # Getting the results and updating the parent self.result variable
         self.result = self.cursor.fetchall()
-
-    def get_most_popular_authors(self):
-        pass
-
-    def get_days_with_most_errors(self):
-        pass
 
 
 def main():
@@ -48,16 +64,26 @@ def main():
     # Calling method to get popular articles
     qp.get_most_popular_articles()
     # Printing the first results..
-    print("Top 3 articles of all time: ")
+    print("********* Top 3 articles of all time: **********")
     for a in qp.result:
         print("'{}' --- {} views".format(a[0], a[1]))
 
     # Calling method to get popular authors
     qp.get_most_popular_authors()
+    print("-------------------------------------------")
+    print("********* Most popular authors of all time:  *********")
+    for a in qp.result:
+        print("{} --- {} views".format(a[0], a[1]))
 
-    # Closing connection
+    # Calling method to get days when failures were > than 1%
+    qp.get_days_with_most_errors()
+    print("-------------------------------------------")
+    print("******** Days with more than 1{} of failures: *******".format('%'))
+    for d in qp.result:
+        print("{} --- {}{} errors".format(d[0], round(float(d[1]), 1), '%'))
+
+    # Closing db connection
     qp.db.close()
-
 
 if __name__ == '__main__':
     main()
